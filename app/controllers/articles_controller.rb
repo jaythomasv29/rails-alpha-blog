264c03f028c279144cfbe3afbc6772f72704462d
James Vongampai <jaythomasv29@gmail.com>
2020-12-04 12:47:07 -0800

    successfully created 'Create' method to allow user to add data to data table and view it dynamically on web page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,20 @@ class ArticlesController < ApplicationController
   def index # controller looks for index.html.erb
     @articles = Article.all
   end
+
+  def create
+    # params[:article]["title"]
+    # @article = Article.new("title": params[:article]["title"], "description": params[:article]["description"])
+    @article = Article.new(params.require(:article).permit(:title, :description)) # require the key of article and permit :title and :description
+    if @article.save # if the article was successfully saved
+      flash[:notice] = "Post was successfully created." # flash helper method
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def new
+    @article = Article.new
+  end
 end
