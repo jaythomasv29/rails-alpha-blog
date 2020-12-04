e81e29427d0a09eef071517b0238e17329c5ece7
James Vongampai <jaythomasv29@gmail.com>
2020-12-04 14:10:41 -0800

    added edit/update feature within the routes=>controller method=>display with views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,6 +8,14 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+ def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end 
+
   def create
     # params[:article]["title"]
     # @article = Article.new("title": params[:article]["title"], "description": params[:article]["description"])
@@ -20,7 +28,14 @@ class ArticlesController < ApplicationController
     end
   end
 
-  def new
-    @article = Article.new
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated successfully"
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
+ 
 end
