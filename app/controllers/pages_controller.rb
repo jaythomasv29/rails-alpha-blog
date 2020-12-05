26d806f633de6162d438546531d815d9a9d24982
James Vongampai <jaythomasv29@gmail.com>
2020-12-05 13:03:43 -0800

    make app production ready to deploy to heroku

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
    def home # home (root) set in routes
+    
   end
 
   def about # from routes /about pages#about =>  about controller to serve about.html.erb
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'pages#home'
+  root 'articles#index'
   get '/about', to: 'pages#about'
   resources :articles #, only: [:show, :index, :create, :new, :edit, :update, :destroy] # only: limits the routes in our app that are exposed
 end
