070ee9401fe753805c65fbed36b5f16f5e4a4f55
James Vongampai <jaythomasv29@gmail.com>
2020-12-06 11:15:45 -0800

    added bootstrap styling

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'articles#index'
   get '/about', to: 'pages#about'
+  get '/home', to: 'pages#home'
   resources :articles #, only: [:show, :index, :create, :new, :edit, :update, :destroy] # only: limits the routes in our app that are exposed
 end
