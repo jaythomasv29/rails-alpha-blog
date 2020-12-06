d146d05fcd31e2151971e97ed44e0763a531e8b0
James Vongampai <jaythomasv29@gmail.com>
2020-12-06 15:54:39 -0800

    added pseudocode for model specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
-
+ # add validations
+ # username must be present and unique, min 3 max 25
+ # email address must be present and unique, max 105
+ # email must be valid email format, checked with regex
+ 
 end
