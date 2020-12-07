9fca8f7e9ff300502f08beab72ed0569039ecc27
James Vongampai <jaythomasv29@gmail.com>
2020-12-07 15:40:27 -0800

    edit user and update user with associated views have been tested and complete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -28,7 +28,6 @@ class ArticlesController < ApplicationController
   end
 
   def update
-  
     if @article.update(article_params)
       flash[:notice] = "Article was updated successfully"
       redirect_to @article
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,13 +9,30 @@ class UsersController < ApplicationController
       flash[:notice] = "Sign up successful"
       redirect_to articles_path
     else
-      render 'new'
+      render "new"
     end
   end
 
-  private 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "User account updated successfully"
+      redirect_to articles_path
+    else
+      render "edit"
+    end
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:id])
+  end
+
   def user_params # private method to white list params
     params.require(:user).permit(:username, :email, :password)
   end
-
 end
