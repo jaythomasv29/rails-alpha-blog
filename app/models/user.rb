6a6204e4192d00ac2c7173cf1580de7cc1aea126
James Vongampai <jaythomasv29@gmail.com>
2020-12-06 16:24:21 -0800

    added user validation to model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,10 @@ class User < ApplicationRecord
  # username must be present and unique, min 3 max 25
  # email address must be present and unique, max 105
  # email must be valid email format, checked with regex
- 
+ validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
+ VALID_EMAIL_REGEX = /(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))\z/
+ validates :email, presence: true, 
+            uniqueness: { case_sensitive: false },
+            format: { with: VALID_EMAIL_REGEX },
+            length: { maximum: 100 }
 end
