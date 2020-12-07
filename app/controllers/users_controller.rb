7411fd5be726abe3213ecd4bc63bdfa2f9c85fc0
James Vongampai <jaythomasv29@gmail.com>
2020-12-07 15:05:14 -0800

    add user create action with associated views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,19 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Sign up successful"
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
+
+  private 
+  def user_params # private method to white list params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
 end
