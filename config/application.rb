ffac452cdb308a3c1c19927f120a285b686f186d
James Vongampai <jaythomasv29@gmail.com>
2020-12-08 20:27:33 -0800

    modified jumbotron and config file to load assets

--- config/application.rb
+++ config/application.rb
@@ -10,8 +10,6 @@ module RailsAlphaBlog
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    config.assets.compile = true
-    config.assets.digest = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
